277b85b6de357ff1810dd5826af8d50d0421a98c
ilona <gogiashvili.ilona@gmail.com>
2022-12-01 13:29:51 -0500

    commit tree 732d784deb97c01a43511c7c4bf1fb44d313de8d

---
+++ build/precache-manifest.f0d9659c3bff8c1791207cc93db93583.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f6704e329e016963009",
+    "url": "/static/css/main.9ccdc86a.chunk.css"
+  },
+  {
+    "revision": "6f6704e329e016963009",
+    "url": "/static/js/main.6f6704e3.chunk.js"
+  },
+  {
+    "revision": "dd7e8a788d3620930dcb",
+    "url": "/static/js/1.dd7e8a78.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8a827b442529e9d4488a91540d010e21",
+    "url": "/index.html"
+  }
+];
